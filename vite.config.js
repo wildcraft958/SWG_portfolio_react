6a8a7e87fd1329fc026099dfa752d5ee1cbd86a4
Animesh (Bakasur) <139889836+wildcraft958@users.noreply.github.com>
2023-12-22 02:04:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://wildcraft958.github.io/SWG_portfolio_react/',
   css : {
     modules : {
       localsConvention : "camelCase"
